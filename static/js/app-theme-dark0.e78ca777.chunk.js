3c10ebc55a2d4e0a6f7780f71067afa88582287a
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2023-09-07 09:59:59 +0400

    deploy pages

---
+++ static/js/app-theme-dark0.e78ca777.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[522],{47696:function(e,a,p){p.r(a),a.default={}}}]);
+//# sourceMappingURL=app-theme-dark0.e78ca777.chunk.js.map
